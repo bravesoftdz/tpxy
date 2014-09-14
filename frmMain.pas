15e5e3d27039f5a42e32aecd332f3270fd414976
Stefan Ascher <stievie2006@gmail.com>
2014-09-14 12:59:08 +0200

    Change stuff

--- frmMain.pas
+++ frmMain.pas
@@ -7,26 +7,26 @@ uses
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, IdContext, System.Actions, Vcl.ActnList,
   Vcl.Menus, Vcl.StdCtrls, IdBaseComponent, IdComponent, IdCustomTCPServer,
   IdTCPServer, IdCmdTCPServer, IdHTTPProxyServer, IdSocksServer, IdIOHandler,
-  IdIOHandlerStream, IdIntercept, IdInterceptThrottler;
+  IdIOHandlerStream, IdIntercept, IdInterceptThrottler, Vcl.ExtCtrls;
 
 type
   TMainForm = class(TForm)
     mLog: TMemo;
-    MainMenu1: TMainMenu;
+    mnuMain: TMainMenu;
     File1: TMenuItem;
     Proxy1: TMenuItem;
-    ActionList1: TActionList;
+    actlMain: TActionList;
     actFileExit: TAction;
     Exit1: TMenuItem;
     actProxyActive: TAction;
     Active1: TMenuItem;
     ipsMain: TIdSocksServer;
-    idThrottle: TIdInterceptThrottler;
     actProxySpeed: TAction;
     MaxSpeed1: TMenuItem;
     Edit1: TMenuItem;
     actEditClear: TAction;
     Clear1: TMenuItem;
+    tiMain: TTrayIcon;
     procedure actFileExitExecute(Sender: TObject);
     procedure actProxyActiveUpdate(Sender: TObject);
     procedure actProxyActiveExecute(Sender: TObject);
@@ -43,10 +43,14 @@ type
     procedure ipsMainConnect(AContext: TIdContext);
     procedure actEditClearExecute(Sender: TObject);
     procedure FormClose(Sender: TObject; var Action: TCloseAction);
+    procedure ipsMainDisconnect(AContext: TIdContext);
   private
     { Private-Deklarationen }
+    FBitsPerSec: Integer;
     procedure SetActive(const AActive: boolean);
     procedure AddLog(const AMsg: string);
+    procedure ReadSettings;
+    procedure WriteSettings;
   public
     { Public-Deklarationen }
   end;
@@ -57,9 +61,12 @@ var
 implementation
 
 uses
-	IdTCPConnection, IdTCPStream, IdGlobal;
+	IdTCPConnection, IdTCPStream, IdGlobal, IniFiles;
 
 {$R *.dfm}
+
+resourcestring
+	SAppTitle = 'Throttle Proxy';
 
 procedure TMainForm.SetActive(const AActive: boolean);
 const
@@ -67,13 +74,35 @@ const
 begin
   if ipsMain.Active <> AActive then begin
   	ipsMain.Active := AActive;
-  	Caption := 'Throttle Proxy ' + Format('[%s]', [ACT[ipsMain.Active]]);
+  	Caption := Format('%s [%s]', [SAppTitle, ACT[ipsMain.Active]]);
+    if AActive then
+    	AddLog(Format('Startup Port %d, %d Bit/s', [ipsMain.DefaultPort, FBitsPerSec]))
+    else
+    	Addlog('Shutdown');
   end;
 end;
 
 procedure TMainForm.AddLog(const AMsg: string);
 begin
 	mLog.Lines.Add(Format('%s: %s', [FormatDateTime('hh:nn:ss', Now), AMsg]));
+end;
+
+procedure TMainForm.ReadSettings;
+begin
+  with TInifile.Create(ChangeFileExt(Application.ExeName, '.ini')) do try
+    FBitsPerSec := ReadInteger('proxy', 'bitspersec', 128000);
+  finally
+    Free;
+  end;
+end;
+
+procedure TMainForm.WriteSettings;
+begin
+  with TInifile.Create(ChangeFileExt(Application.ExeName, '.ini')) do try
+    WriteInteger('proxy', 'bitspersec', FBitsPerSec);
+  finally
+    Free;
+  end;
 end;
 
 procedure TMainForm.actEditClearExecute(Sender: TObject);
@@ -101,12 +130,12 @@ var
 	val: string;
   act: boolean;
 begin
-	val := IntToStr(idThrottle.BitsPerSec);
-	if InputQuery('Max Speed', 'Max Speed in Bits per sec.', val) then begin
+	val := IntToStr(FBitsPerSec);
+	if InputQuery('Max Speed', 'Max Speed in Bits per sec. 0 = unlimited.', val) then begin
   	act := ipsMain.Active;
     if act then
 			SetActive(false);
-		idThrottle.BitsPerSec := StrToInt(val);
+		FBitsPerSec := StrToInt(val);
     if act then
 			SetActive(true);
   end;
@@ -115,49 +144,64 @@ end;
 procedure TMainForm.FormClose(Sender: TObject; var Action: TCloseAction);
 begin
 	SetActive(false);
+  WriteSettings;
 end;
 
 procedure TMainForm.FormCreate(Sender: TObject);
 begin
-//	ipsMain.Intercept := IdInterceptThrottler1;
+	Caption := SAppTitle;
+	FBitsPerSec := 128000;
+  ReadSettings;
 	SetActive(true);
 end;
 
 procedure TMainForm.idThrottleConnect(
   ASender: TIdConnectionIntercept);
 begin
-	AddLog('Intercept Connect');
+//	AddLog('Intercept Connect');
 end;
 
 procedure TMainForm.idThrottleDisconnect(
   ASender: TIdConnectionIntercept);
 begin
-	AddLog('Intercept Disconnect');
+//	AddLog('Intercept Disconnect');
 end;
 
 procedure TMainForm.idThrottleReceive(
   ASender: TIdConnectionIntercept; var ABuffer: TArray<System.Byte>);
 begin
-	AddLog(Format('Receive: %d B', [Length(ABuffer)]));
+//	AddLog(Format('Receive: %d B', [Length(ABuffer)]));
 end;
 
 procedure TMainForm.idThrottleSend(ASender: TIdConnectionIntercept;
   var ABuffer: TArray<System.Byte>);
 begin
-	AddLog(Format('Send: %d B', [Length(ABuffer)]));
+//	AddLog(Format('Send: %d B', [Length(ABuffer)]));
 end;
 
 procedure TMainForm.ipsMainBeforeSocksConnect(AContext: TIdSocksServerContext;
   var VHost: string; var VPort: Word; var VAllowed: Boolean);
+var
+	Throttle: TIdInterceptThrottler;
 begin
 	AddLog(Format('SOCKS%d: %s:%d', [AContext.SocksVersion, VHost, VPort]));
-  AContext.Connection.Intercept := idThrottle;
+  Throttle := TIdInterceptThrottler.Create(AContext.Connection);
+  Throttle.BitsPerSec := FBitsPerSec;
+  Throttle.OnConnect := idThrottleConnect;
+  Throttle.OnDisconnect := idThrottleDisconnect;
+  Throttle.OnReceive := idThrottleReceive;
+  Throttle.OnSend := idThrottleSend;
+  AContext.Connection.Intercept := Throttle;
 end;
 
 procedure TMainForm.ipsMainConnect(AContext: TIdContext);
 begin
-	AddLog('Connect');
-  TIdSocksServerContext(AContext).Connection.Intercept := idThrottle;
+//	AddLog('Connect');
+end;
+
+procedure TMainForm.ipsMainDisconnect(AContext: TIdContext);
+begin
+//	AddLog('Disconnect');
 end;
 
 end.
